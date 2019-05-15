ca0cd9801abd6e82a70018747c291ace97dad80a
Samuel Cotter <swcotter@comcast.net>
2019-05-14 19:20:03 -0700

    Removed Jumbotron and replaced it with a container

---
+++ assets/Javascript/App.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDzxmx6cf6vlF-W-6G5lCj8WNb-5_lzJW4",
+    authDomain: "project-1-937d6.firebaseapp.com",
+    databaseURL: "https://project-1-937d6.firebaseio.com",
+    projectId: "project-1-937d6",
+    storageBucket: "project-1-937d6.appspot.com",
+    messagingSenderId: "1011145173029",
+    appId: "1:1011145173029:web:ac65f04603865158"
+  };
