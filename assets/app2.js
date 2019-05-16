255595f568dfde0f5c96eb7746e5d3d6a2fd6ad6
Surafel Ferede <surafelaron@gmail.com>
2019-05-16 16:04:48 -0700

    functional api calls to NPS and weather!

---
+++ assets/app2.js
@@ -0,0 +1,50 @@
+// $(document).ready(function(){
+    // $("#getWeather").click(function(){
+    // var zipCode = response.data[j].addresses[0].postalCode;
+    // event.preventDefault();
+   
+
+    
+    // if(city != ""){
+       //this api call weather
+    //    $.ajax({
+    //        url:'http://api.openweathermap.org/data/2.5/weather?q=' + zipCode + "&units=imperial" + "&APPID=0d662bff66bb9cd3cbf1260d8c1c69d5",
+    //        type: "GET",
+    //        dataType:"jsonp"})
+    //        .then(function(data){
+    //            console.log(data)
+    //        })
+        // });
+    
+        //        //creating elements
+        //        var widget = show(data);
+    
+        //        //select and append created elemts to parent edlement
+        //        $("#showweather").html(widget);
+    
+        //        console.log(data);
+        //         // $("#city").val("") ;
+        //    }
+    
+    
+    //    });
+    // }else{
+    //    $("#error").html("field cannot be empty");
+    // }
+    
+    // });
+    
+    // });
+    
+    // function show(data) {
+    //    return  "<h2><strong>current weather for : </strong>" + data.name +"," + data.sys.country + "</h2>" +
+    //            "<h4><strong>Weather: </strong>" + data.weather[0].main + "</h4>" +
+    //            "<h4><strong>Description:</strong><img src='http://openweathermap.org/img/w/" + data.weather[0].icon +".png'>"
+    //             + data.weather[0].description + "</h4>" +
+    //            "<h4><strong>Humidity: </strong>" + data.main.humidity + "%</h4>" +
+    //            "<h4> <strong>Windspeed: </strong> " + data.wind.speed + "m/s</h4>" +
+    //            "<h4> <strong>Temprature: </strong>" + data.main.temp + "&deg;c" + "</h4>" ;
+    
+    
+    
+    // 
